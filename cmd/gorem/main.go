1a53323022a0da1630af86636b141b5a1164dbea
Pascal Bättig <pbaettig@gmail.com>
2020-02-24 20:59:14 +0100

    add signal handling, add metrics server for prometheus metrics, run servers in separate goroutines

--- cmd/gorem/main.go
+++ cmd/gorem/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	log "github.com/sirupsen/logrus"
@@ -16,10 +21,47 @@ func init() {
 	log.SetLevel(log.DebugLevel)
 }
 
+func mainServer(h http.Handler, errs chan<- error) *http.Server {
+	srv := &http.Server{
+		Addr:         "0.0.0.0:8080",
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+		IdleTimeout:  time.Second * 60,
+		Handler:      h,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			errs <- fmt.Errorf("main server: %w", err)
+		}
+	}()
+
+	return srv
+}
+
+func metricsServer(errs chan<- error) *http.Server {
+	srv := &http.Server{
+		Addr:         "0.0.0.0:9100",
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+		IdleTimeout:  time.Second * 60,
+		Handler:      promhttp.Handler(),
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			errs <- fmt.Errorf("metrics server: %w", err)
+		}
+	}()
+
+	return srv
+}
+
 func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+
 	root := mux.NewRouter()
 	config := root.PathPrefix("/config").Subrouter()
-	internal := root.PathPrefix("/internal").Subrouter()
+	// internal := root.PathPrefix("/internal").Subrouter()
 
 	root.Use(middleware.Log)
 	root.Handle("/", handlers.HelloWorld)
@@ -33,8 +75,29 @@ func main() {
 	config.Handle("/health", handlers.HealthConfig)
 	config.Use(middleware.Authenticate)
 
-	internal.Handle("/metrics", promhttp.Handler())
+	srvError := make(chan error)
+	mainSrv := mainServer(root, srvError)
+	metricSrv := metricsServer(srvError)
 
-	http.Handle("/", root)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	for {
+		select {
+		case s := <-sigs:
+			log.Debugf("Singal '%s' received", s.String())
+
+			// create a context to wait for open connections when shutting down servers
+			ctx, cancel := context.WithTimeout(context.Background(), time.Duration(15*time.Second))
+			defer cancel()
+
+			log.Info("shutting down servers")
+			mainSrv.Shutdown(ctx)
+			metricSrv.Shutdown(ctx)
+
+			log.Info("goodbye")
+			os.Exit(0)
+
+		case err := <-srvError:
+			// exit with an error if any one of the servers failed to start
+			log.Fatal(err.Error())
+		}
+	}
 }
